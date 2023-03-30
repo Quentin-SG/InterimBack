f43d31545c6123f113e9d101d48631b259ee7445
qsguily <qsguily@gmail.com>
2023-03-30 12:49:20 +0200

    add route aut and login

--- server.js
+++ server.js
@@ -1,44 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser"
-import dotenv from "dotenv";
-import mysql from "mysql2";
-import { scryptSync, randomBytes, timingSafeEqual } from "crypto";
-dotenv.config();
+
+import authentificationRoutes from "./routes/authentification.js";
 
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
 const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use( "/aut", authentificationRoutes );
 
 app.listen( PORT, () => {
     console.log("Server started and listening on port " + PORT);
 });
-
-const pool = mysql.createPool({
-    host: process.env.MYSQL_ADDON_HOST,
-    user: process.env.MYSQL_ADDON_USER,
-    port: process.env.MYSQL_ADDON_PORT,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB
-}).promise();
-
-
-app.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = (await pool.query(`select id, mdp, salt from utilisateur where email = ?;`, [email]))[0][0];
-
-    if( user ){
-
-        const hashedBuffer = scryptSync( password, user.salt, 64 );
-        const keyBuffer = Buffer.from( user.mdp, "hex" );
-
-        if( timingSafeEqual( hashedBuffer, keyBuffer ) ) res.status(200).send("Succesfully connected");
-        else res.status(401).send("Wrong password");
-        
-    }else res.status(401).send("Wrong email");
-
-});
-
-
