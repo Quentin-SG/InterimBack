90259231863647a887e95beeac3ce8e0ccc4db2a
qsguily <qsguily@gmail.com>
2023-03-30 12:36:44 +0200

    add login POST

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser"
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import { scryptSync, randomBytes, timingSafeEqual } from "crypto";
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const PORT = 5000;
 
@@ -20,15 +24,21 @@ const pool = mysql.createPool({
 }).promise();
 
 
-app.get("/user", async (req, res) => {
+app.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = (await pool.query(`select id, mdp, salt from utilisateur where email = ?;`, [email]))[0][0];
 
-    const email = req.query.email;
-    const rows = await pool.query(`select mdp, salt from utilisateur where email = ?;`, [email]);
-    res.status(201).send(rows[0]);
+    if( user ){
+
+        const hashedBuffer = scryptSync( password, user.salt, 64 );
+        const keyBuffer = Buffer.from( user.mdp, "hex" );
+
+        if( timingSafeEqual( hashedBuffer, keyBuffer ) ) res.status(200).send("Succesfully connected");
+        else res.status(401).send("Wrong password");
+        
+    }else res.status(401).send("Wrong email");
 
 });
-
-
-
 
 
