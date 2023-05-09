cf387972f9a95477d75d6da320f04980c7acaa1a
karlaB98 <karlaclaude@gmail.com>
2023-05-09 16:17:57 +0200

    added notification router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser"
 
 import authentificationRoutes from "./routes/authentification.js";
+import notificationRoutes from "./routes/notification.js";
 
 const app = express();
 const PORT = 5000;
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use( "/aut", authentificationRoutes );
+app.use( "/not", notificationRoutes );
 
 app.listen( PORT, () => {
     console.log("Server started and listening on port " + PORT);
