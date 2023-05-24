438676e16d94c447e9fdad00043e19674c773863
karlaB98 <karlaclaude@gmail.com>
2023-05-24 13:22:33 +0200

    general fixes + duplicate id error handling

--- routes/authentification.js
+++ routes/authentification.js
@@ -53,13 +53,56 @@ router.post("/signup-jobseeker", async ( req, res ) => {
     const nationality  = req.body.nationality;
 
     const user = (await pool.query(`select id from utilisateur where id = ?;`, [userId]))[0][0];
+    const subuser = (await pool.query(`select id_utilisateur from utilisateurPayant where id_utilisateur = ?;`, [userId]))[0][0];
+
+    if( subuser ) res.status(401).send([{message:"Bad user"}]);
 
     if( user ){
         if( (await pool.query(`insert into demandeurEmploi values (?,?,?,?,?,?);`, [ userId, firstname, lastname, birthday, city, nationality]))[0].affectedRows ){
             await pool.query(`update utilisateur set userRole = 1 where id = ?;`, [ userId ]);
-            res.status(201).send({message:"Successfully registered", userId});
+            res.status(201).send([{message:"Successfully registered", userId}]);
         }
-    }
+    }else res.status(401).send([{message:"Bad user"}]);
+    
+});
+
+router.post("/signup-sub", async ( req, res ) => {
+    const userId = req.body.id;
+    const numberN = req.body.numberN;
+    const lastname1 = req.body.lastname1;
+    const lastname2 = req.body.lastname2;
+    const mail2 = req.body.mail2;
+    const phone1 = req.body.phone1;
+    const phone2 = req.body.phone2;
+    const adress = req.body.adress;
+    const link = req.body.link;
+    const type = parseInt(req.body.type);
+
+    const user = (await pool.query(`select id from utilisateur where id = ?;`, [userId]))[0][0];
+    const subuser = (await pool.query(`select id_utilisateur from utilisateurPayant where id_utilisateur = ?;`, [userId]))[0][0];
+
+    if( subuser ) res.status(401).send([{message:"Bad user"}]);
+
+    if( user ){
+        if( (await pool.query(`insert into utilisateurPayant values (?,?,?,?,?,?,?,?,?,?);`, [ userId, numberN, lastname1, lastname2, mail2, phone1, phone2, adress, link, null ]))[0].affectedRows ){
+            await pool.query(`update utilisateur set userRole = ? where id = ?;`, [ type, userId ]);
+
+            if( type === 2 ){
+                const businessName = req.body.businessName;
+                const depServ = req.body.depServ;
+                const depSServ = req.body.depSServ;
+                if( (await pool.query(`insert into employeur values (?,?,?,?);`, [userId, businessName, depServ, depSServ ]))[0].affectedRows ){
+                    res.status(201).send([{message:"Successfully registered", userId}]);
+                }
+            }else if( type === 3 ){
+                const agencyName = req.body.agencyName;
+                if( (await pool.query(`insert into agence values (?,?,?);`, [userId, agencyName, null ]))[0].affectedRows ){
+                    res.status(201).send([{message:"Successfully registered", userId}]);
+                }
+            }
+            
+        }
+    }else res.status(401).send([{message:"Bad user"}]);
     
 });
 
