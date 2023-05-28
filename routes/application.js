471e2a7cf1c93017117d4f5d97f77caf692e072f
qsguily <qsguily@gmail.com>
2023-05-28 16:52:30 +0200

    init

---
+++ routes/application.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { pool } from "../database.js";
+import { v4 as uuidv4 } from "uuid";
+
+const router = express.Router();
+
+export default router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import authentificationRoutes from "./routes/authentification.js";
 import notificationRoutes from "./routes/notification.js";
 import userRoutes from "./routes/user.js";
 import offerRoutes from "./routes/offer.js";
+import applicationRoutes from "./routes/offer.js";
 
 const app = express();
 const PORT = 5000;
@@ -16,6 +17,7 @@ app.use( "/aut", authentificationRoutes );
 app.use( "/not", notificationRoutes );
 app.use( "/user", userRoutes );
 app.use( "/offer", offerRoutes );
+app.use( "/app", applicationRoutes );
 
 app.listen( PORT, () => {
     console.log("Server started and listening on port " + PORT);
