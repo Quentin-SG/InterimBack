3a91e73bf7e1a0f447deb5b339ca8b95df02bc73
qsguily <qsguily@gmail.com>
2023-05-28 12:24:00 +0200

    added create route

--- routes/offer.js
+++ routes/offer.js
@@ -1,6 +1,17 @@
 import express from "express";
 import { pool } from "../database.js";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
+
+router.post( "/create", async (req, res) => {
+    const id = uuidv4();
+    const name = req.body.name;
+    const id_user = req.body.id_user;
+    const views = 0;
+    if( (await pool.query(`insert into offre values (?,?,?,?);`, [ id, name, id_user, views ]))[0].affectedRows ){
+        res.status(201).send([{message:"Successfully created", id}]);
+    }
+});
 
 export default router;
